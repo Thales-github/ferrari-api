e5d3f063829ef7761e41365e80449637fc3e0d31
Thales-github <thalesvitoroliveiracirino@gmail.com>
2023-05-11 22:30:55 -0300

    até aqui

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common';
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [],
 })
